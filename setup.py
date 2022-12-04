10ad9e0def538ea2755f6a688a1402bf34e43ef8
kohei <koheitech001@gmail.com>
2022-12-04 20:17:04 +0900

    update setup

--- setup.py
+++ setup.py
@@ -1,10 +1,31 @@
 from setuptools import setup, find_packages
 
+def take_package_name(name):
+    if name.startswith("-e"):
+        return name[name.find("=")+1:name.rfind("-")]
+    else:
+        return name.strip()
+
+def load_requires_from_file(filepath):
+    with open(filepath) as fp:
+        return [take_package_name(pkg_name) for pkg_name in fp.readlines()]
+
+def load_links_from_file(filepath):
+    res = []
+    with open(filepath) as fp:
+        for pkg_name in fp.readlines():
+            if pkg_name.startswith("-e"):
+                res.append(pkg_name.split(" ")[1])
+    return res
+
 setup(
     name='MediaHandler',\
     version="0.0.1",\
     description="",\
     author='kevin-tofu',\
     packages=find_packages(), \
-    license='MIT'
+    license='MIT', \
+    install_requires=load_requires_from_file("requirements.txt"),
+    dependency_links=load_links_from_file("requirements.txt"),
+
 )
