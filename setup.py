8bb12d54aea070ef2a6d1d10235901384bd2e4c6
kohei <koheitech001@gmail.com>
2022-12-05 20:14:14 +0900

    update

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     name='MediaHandler',\
     version="0.0.1",\
     description="",\
+    packages=['MediaHandler'],\
+    package_dir = {'MediaHandler': 'MediaHandler'},\
     author='kevin-tofu',\
     packages=find_packages(), \
     license='MIT', \
