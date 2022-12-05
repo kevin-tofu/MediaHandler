e767226e848a405a9684b63c8e5c7fa69e5b50b1
kohei <koheitech001@gmail.com>
2022-12-05 20:30:53 +0900

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 print(find_packages())
 setup(
     name='MediaHandler',\
-    version="0.0.1",\
+    version="v0.0.1",\
     url='https://github.com/kevin-tofu/MediaHandler',\
     download_url='https://github.com/kevin-tofu/MediaHandler/releases/tag/v0.0.1',\
     description="",\
@@ -12,4 +12,5 @@ setup(
     package_dir = {'MediaHandler': 'MediaHandler'},\
     author='kevin-tofu',\
     license='MIT', \
+    python_requires='>3.8'
 )
