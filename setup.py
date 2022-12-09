9721b479e21d91ce0b533e1b361ec83ae0e09c6f
kohei <koheitech001@gmail.com>
2022-12-09 19:56:14 +0900

    up

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
 from setuptools import setup, find_packages
+from typing import List
 
 print(find_packages())
+
+def get_install_requires() -> List[str]:
+    """Returns requirements.txt parsed to a list"""
+    fname = Path(__file__).parent / 'requirements.txt'
+    targets = []
+    if fname.exists():
+        with open(fname, 'r') as f:
+            targets = f.read().splitlines()
+    return targets
+
 setup(
     name='MediaHandler',\
     version="v0.0.1",\
     url='https://github.com/kevin-tofu/MediaHandler',\
     download_url='https://github.com/kevin-tofu/MediaHandler/releases/tag/v0.0.1',\
     description="",\
+    install_requires=get_install_requires(),
     packages=['MediaHandler'],\
     # packages=find_packages(), \
     package_dir = {'MediaHandler': 'MediaHandler'},\
