7d1d21ccb2dc83ad4e7e42821e0b435aad42f6a2
kohei <koheitech001@gmail.com>
2022-12-10 12:08:08 +0900

    update

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ print(find_packages())
 
 def get_install_requires() -> List[str]:
     """Returns requirements.txt parsed to a list"""
-    fname = pathlib.Path(__file__).parent / 'requirements.txt'
+    fname = f"{pathlib.Path(__file__).parent}/requirements.txt"
     targets = []
     if fname.exists():
         with open(fname, 'r') as f:
@@ -21,8 +21,8 @@ setup(
     url='https://github.com/kevin-tofu/MediaHandler',\
     download_url='https://github.com/kevin-tofu/MediaHandler/releases/tag/v0.0.1',\
     description="",\
-    # install_requires=get_install_requires(),
-    install_requires=['fastapi', 'uvicorn', 'wheel'],
+    install_requires=get_install_requires(),
+    # install_requires=['fastapi', 'uvicorn', 'wheel'],
     packages=['MediaHandler'],\
     # packages=find_packages(), \
     package_dir = {'MediaHandler': 'MediaHandler'},\
